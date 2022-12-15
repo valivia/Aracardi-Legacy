570ab03a8c06c8bbaceef40453023e2371a0cfca
valivia <valivia@xayania.com>
2022-12-15 18:26:55 +0000

    superjson

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,6 +1,7 @@
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import type { AppRouter } from "@server/routers/_app";
+import Superjson from "superjson";
 
 function getBaseUrl() {
   if (typeof window !== "undefined")
@@ -14,23 +15,11 @@ function getBaseUrl() {
 export const trpc = createTRPCNext<AppRouter>({
   config() {
     return {
+      transformer: Superjson,
       links: [
-        httpBatchLink({
-          /**
-           * If you want to use SSR, you need to use the server's full URL
-           * @link https://trpc.io/docs/ssr
-           **/
-          url: `${getBaseUrl()}/api/trpc`,
-        }),
+        httpBatchLink({ url: `${getBaseUrl()}/api/trpc` }),
       ],
-      /**
-       * @link https://tanstack.com/query/v4/docs/reference/QueryClient
-       **/
-      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
     };
   },
-  /**
-   * @link https://trpc.io/docs/ssr
-   **/
   ssr: false,
 });
