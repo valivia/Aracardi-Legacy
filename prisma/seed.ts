58be6d3423ade0dd94d90c72b374cf08fff0dc81
valivia <valivia@xayania.com>
2022-12-15 20:44:27 +0000

    Made the seed function insert random data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,64 @@
-import { Addon, PrismaClient, StageType } from "@prisma/client";
-
-function createCards(addon: Addon) {
-  const cards = [];
-  const range = Math.floor(Math.random() * (300 - 30 + 1)) + 30;
-  for (let index = 0; index < range; index++) {
-    cards.push({
-      addon_id: addon.id,
-      is_nsfw: Math.random() < 0.5,
-      stages: [
-        {
-          title: `card_${addon.title}_${index}`,
-          text: "test",
-          type: StageType.TEXT,
-        },
-      ],
-    });
-  }
-  return cards;
-}
+import { Addon, Game, PrismaClient, StageType } from "@prisma/client";
+import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
+const createArray = (min: number, max: number) => faker.datatype.array(faker.datatype.number({ min, max }));
+
+function createTextStage() {
+  const a = faker.lorem.sentence()
+    .split(" ")
+    .map(x => Math.random() < 0.1
+      ? faker.helpers.arrayElement(["%PLAYER1%", "%SELF%", "%TURNS%"])
+      : x
+    );
+
+  const hasTurns = a.indexOf("%TURNS%") !== -1;
+  const text = a.join(" ");
+
+  return {
+    type: StageType.TEXT,
+    title: faker.random.words(5),
+    text,
+    turns: hasTurns ? faker.datatype.number(12) : undefined,
+  };
+}
+
+function createPollStage() {
+  return {
+    type: StageType.POLL,
+    title: faker.random.words(5),
+    winner_points: faker.datatype.boolean() ? faker.datatype.number(1000) : null,
+    selection_count: 1,
+    options: createArray(2, 7).map(() => ({ text: faker.lorem.sentence() })),
+  };
+}
+
+function createCard(addon: Addon) {
+  return {
+    addon_id: addon.id,
+    is_nsfw: faker.datatype.boolean(),
+    stages: createArray(1, 5).map(() => faker.datatype.boolean() ? createTextStage() : createPollStage()),
+  };
+}
+
+function createAddon(game: Game) {
+  return {
+    created_at: faker.date.past(2),
+    updated_at: faker.date.past(),
+
+    title: faker.random.words(2),
+    description: faker.lorem.sentence(),
+
+    is_official: faker.datatype.boolean(),
+    has_image: faker.datatype.boolean(),
+    is_draft: faker.datatype.boolean(),
+
+    is_available_online: faker.datatype.boolean(),
+    is_available_offline: faker.datatype.boolean(),
+
+    game_id: game.id,
+  };
+}
 
 async function main() {
   await prisma.game.create({
@@ -63,7 +102,7 @@ async function main() {
     },
   });
 
-  await prisma.game.create({
+  const game = await prisma.game.create({
     data: {
       title: "Tipsy Sailor",
       description: "gamer gaming game",
@@ -75,31 +114,22 @@ async function main() {
       is_available_offline: false,
 
       authors: { create: [{ name: "Julien" }, { name: "Tetro" }] },
-
-      addons: {
-        createMany: {
-          data: [
-            {
-              title: "Base Pack",
-              description: "The default set of cards",
-              is_official: true,
-              has_image: true,
-              is_draft: false,
-              is_available_offline: true,
-              is_available_online: true,
-            },
-          ],
-        },
-      },
     },
   });
 
+
+  // create addons.
+  await prisma.addon.createMany({
+    data: createArray(10, 30).map(() => createAddon(game)),
+  });
+
+  // create cards.
   const addons = await prisma.addon.findMany();
-
   for (const addon of addons) {
-    await prisma.card.createMany({ data: createCards(addon) });
-  }
+    const data = createArray(50, 300).map(() => createCard(addon));
 
+    await prisma.card.createMany({ data });
+  }
 }
 
 main()
