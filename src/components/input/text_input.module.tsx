2d0f72e66a5240c8a411d59f5a315b5241fa2866
valivia <valivia@xayania.com>
2022-12-30 23:57:24 +0000

    im so fucking stupid (fixed text input)

--- src/components/input/text_input.module.tsx
+++ src/components/input/text_input.module.tsx
@@ -4,6 +4,8 @@ import styles from "./text_input.module.scss";
 
 const TextInput: React.FC<Props> = ({
   placeholder,
+  value,
+  onChange,
   size = "normal",
 }) => {
 
@@ -12,6 +14,8 @@ const TextInput: React.FC<Props> = ({
       type={"search"}
       className={styles.main}
       placeholder={placeholder}
+      value={value}
+      onChange={onChange}
 
       data-size={size}
     />
@@ -22,7 +26,7 @@ export { TextInput };
 
 interface Props {
   value: string;
-  onChange: (e: any) => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   size?: "small" | "normal" | "big";
   placeholder: string;
 }
