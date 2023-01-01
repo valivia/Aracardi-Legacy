71b9419391163216537f804572e48531a7cdd92c
valivia <valivia@xayania.com>
2023-01-01 00:47:27 +0000

    home page UI start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,61 @@
 import styles from "./index.module.scss";
 import { Layout } from "src/components/global/layout.module";
-import { Game } from "@structs/game";
-import { prisma } from "src/server/prisma";
+import { TextInput } from "@components/input/text_input.module";
+import { Button } from "@components/input/button.module";
+import { useState } from "react";
+import { Link } from "@components/input/link.module";
 import { trpc } from "@utils/trpc";
-import GameComponent from "@components/index/game.module";
 
-const Home = ({ games }: Props) => {
-  const test = trpc.game.all.useQuery({});
-  if (test.data) console.log({ test });
-  console.log({ games });
+const Home = () => {
+  const [joinCode, setJoinCode] = useState("");
+
+  const fetchSession = async () => {
+    // TODO make this not be broken
+    const session = await trpc.session.getByJoinCode.useQuery({ join_code: joinCode });
+    console.log(session);
+  };
 
   return (
-    <Layout>
+    <Layout title="Aracardi">
       <main className={styles.main}>
-        {games && games.map((game) =>
-          <GameComponent
-            key={game.id}
-            game={game}
-          />
-        )}
+
+        <h1 className={styles.title}>Play now!</h1>
+        <hr />
+
+        <form className={styles.form}>
+
+          {/* Join game */}
+          <fieldset className={styles.joinGame}>
+            <TextInput
+              value={joinCode}
+              onChange={(e) => setJoinCode(e.target.value)}
+
+              size="lg"
+              placeholder="Join Code"
+            />
+            <Button
+              onClick={fetchSession}
+              size="lg"
+              variant="secondary"
+            >
+              Join Game
+            </Button>
+          </fieldset>
+
+          <hr />
+
+          {/* Create game */}
+          <Link
+            size="lg"
+            href="/game"
+          >
+            Create Game
+          </Link>
+        </form>
+
       </main>
     </Layout>
   );
 };
 
-interface Props {
-  games: Game[]
-}
-
 export default Home;
-
-export async function getStaticProps() {
-  const result = await prisma.game.findMany();
-  const games = result.map(game => ({ ...game, created_at: Number(game.created_at), updated_at: Number(game.updated_at) }));
-  return { props: { games } };
-
-}
