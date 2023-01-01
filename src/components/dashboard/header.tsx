948ebda81614dd641f6ad8f44d33fc0653f9105e
valivia <valivia@xayania.com>
2023-01-01 23:25:39 +0000

    feat: dashboard header

---
+++ src/components/dashboard/header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./banner.module.scss";
+
+const Header: React.FC<Props> = ({ title, description, avatar: Avatar }) => {
+  return (
+    <header className={styles.main}>
+      {Avatar && <figure className={styles.avatar}><Avatar /></figure>}
+      {title && <h1 className={styles.title}>{title}</h1>}
+      {description && <p className={styles.description}>{description}</p>}
+      <hr className={styles.line} />
+    </header>
+  );
+};
+
+export { Header };
+
+interface Props {
+  title?: string;
+  description?: string;
+  avatar: any;
+}
