4857f4a62b3faab7b84a0f530772f0bdfb6b1462
valivia <valivia@xayania.com>
2023-01-09 17:25:39 +0000

    feat: dashboard item

---
+++ src/components/dashboard/dashboard_item.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import React, { PropsWithChildren } from "react";
+import { BsGear } from "react-icons/bs";
+import styles from "./dashboard_item.module.scss";
+
+// TODO settings?
+
+const DashboardItem: React.FC<PropsWithChildren<Props>> = ({ title, href, avatar, children }) => {
+  return (
+    <Link
+      href={href}
+      className={styles.main}
+      tabIndex={0}
+    >
+
+      {/* Avatar */}
+      <figure className={styles.avatar}>{avatar}</figure>
+
+      {/* Info */}
+      <section className={styles.info}>
+        <h2 className={styles.title}>{title}</h2>
+
+        {/* Tags */}
+        <section className={styles.tags}>
+          {children}
+        </section>
+
+      </section>
+
+      {/* Settings */}
+      <div className={styles.settings} onClick={(e) => { e.stopPropagation(); e.nativeEvent.preventDefault(); }}>
+        <BsGear />
+      </div>
+    </Link>
+  );
+};
+
+export { DashboardItem };
+
+interface Props {
+  title: string;
+  href: string;
+  avatar: any;
+}
