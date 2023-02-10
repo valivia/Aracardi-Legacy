1ee336e1efaf605632913ad8289bb72c88e197dd
valivia <valivia@xayania.com>
2023-02-10 16:32:56 +0000

    added select element

---
+++ src/components/input/select.tsx
@@ -0,0 +1,85 @@
+import styles from "./select.module.scss";
+import * as Radix from "@radix-ui/react-select";
+import React from "react";
+import { BsCheck, BsChevronDown, BsChevronUp } from "react-icons/bs";
+
+const Select: React.FC<SelectProps> = ({ defaultValue, value, onValueChange, disabled, items }) => {
+  return (
+    <Radix.Root
+      defaultValue={defaultValue}
+      value={value}
+      onValueChange={onValueChange}
+    >
+      {/* Trigger */}
+      <Radix.Trigger
+        className={styles.trigger}
+        disabled={disabled}
+      >
+        <Radix.Value />
+        <Radix.Icon>
+          <BsChevronDown />
+        </Radix.Icon>
+      </Radix.Trigger>
+
+      {/* Content */}
+      <Radix.Portal>
+        <Radix.Content
+          className={styles.content}
+          onBlur={(e) => e.preventDefault()}
+        >
+
+          <Radix.ScrollUpButton className={styles.scrollButton}>
+            <BsChevronUp />
+          </Radix.ScrollUpButton>
+
+          {/* Items */}
+          <Radix.Viewport className={styles.viewport}>
+
+            {items.map((item, index) => {
+              return (
+                <Radix.SelectItem
+                  key={index}
+                  value={item.value}
+                  className={styles.item}
+                >
+
+                  <Radix.SelectItemText>
+                    {item.label}
+                  </Radix.SelectItemText>
+
+                  <Radix.SelectItemIndicator className={styles.indicator}>
+                    <BsCheck />
+                  </Radix.SelectItemIndicator>
+
+                </Radix.SelectItem>
+              );
+            })}
+
+          </Radix.Viewport>
+
+          <Radix.ScrollDownButton className={styles.scrollButton}>
+            <BsChevronDown />
+          </Radix.ScrollDownButton>
+
+        </Radix.Content>
+      </Radix.Portal>
+
+
+    </Radix.Root>
+  );
+};
+
+interface SelectProps {
+  defaultValue?: string;
+  value?: string;
+  onValueChange?: (value: string) => void;
+  disabled?: boolean;
+  items: {
+    label: string;
+    value: string;
+    disabled?: boolean;
+  }[];
+}
+
+
+export { Select };
--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,8 +1,30 @@
+import { Button } from "@components/input/button";
+import { Select } from "@components/input/select";
+import { SelectGroup, SelectItem } from "@radix-ui/react-select";
 import { NextPage } from "next";
+import React from "react";
 
 const Demo: NextPage = () => {
+  const [value, setValue] = React.useState("foo");
+
   return (
-    <></>
+    <>
+      <Button>nuts</Button>
+      <Select
+        defaultValue={value}
+        value={value}
+        items={[
+          { label: "foo", value: "foo", disabled: false },
+          { label: "bar", value: "bar", disabled: true },
+          { label: "baz", value: "baz", disabled: false },
+          { label: "qux", value: "qux", disabled: false },
+          { label: "kuux", value: "kuux", disabled: false },
+        ]}
+        onValueChange={setValue}
+      />
+
+      <Button>nuts</Button>
+    </>
   );
 };
 
